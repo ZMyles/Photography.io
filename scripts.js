d4dbac8e2fd0e1cf0cd4cc3ea609d8a379cda021
Zavier <zmyles@comcast.net>
2020-02-17 19:21:04 -0500

    Almost done, Need to find new way to you PHP for Email use and touch up UI Design.

--- scripts.js
+++ scripts.js
@@ -13,44 +13,108 @@ $(window).on('scroll', function() {
 
 $(window).on('scroll', function() {
   $(this).css("text-shadow","2px 2px red")
-})
+});
 
 
 /* =============================================================
 =========== Contact form code ===================
 ================================================================*/
 
+$(document).ready(function(){
+  $('#submit').click(function() {
+    var name = $("#name").val();
+    var email =$('#email').val();
 
-$(document).ready(function () {
-   $('.submit').click(function(event) {
-     
-     
-     var email = $('.email').val()
-     var subject = $('.subject').val()
-     var message = $('.message').val()
-     var statusElm = $('.status')
-     statusElm.empty()//Empty's function so we always start from scratch
+    var data = 'name=' + name + '&email=' + email;
+    console.log(data);
 
-     if(email.length > 5 && email.includes('@') && email.includes('.')) {
-       statusElm.append('<div>Email is valid</div>')//Adds the new HTML to inform user
-      } else {
-        event.preventDefault()//stops browser from triggering the form submit
-        statusElm.append('<di>Email is not valid</div>')//Adds the new HTML to inform user
+    $.ajax({
+      type: "POST",
+      url: 'php.php',
+      data: varData,
+      success: function(){
+        alert("it was a success");
       }
 
-      if(subject.length >= 2){
-        statusElm.append('<div>Subject is valid</div>')
-      } else {
-        event.preventDefault()//stops browser from triggering the form submit
-        statusElm.append('<div>Subject is not valid</div>')
-      }
+    });
 
-      if(message.length >= 10) {
-        statusElm.append('<div>Subject is valid</div>')
-      } else {
-        event.preventDefault()//stops browser from triggering the form submit
-        statusElm.append('<div>Subject is not valid</div>')
-      }
-   })
+  });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//    $('.submit').click(function(event) {
+     
+     
+//      var email = $('.email').val()
+//      var subject = $('.subject').val()
+//      var message = $('.message').val()
+//      var statusElm = $('.status')
+//      statusElm.empty()//Empty's function so we always start from scratch
+
+//      if(email.length > 5 && email.includes('@') && email.includes('.')) {
+//        statusElm.append('<div>Email is valid</div>')//Adds the new HTML to inform user
+//       } else {
+//         event.preventDefault()//stops browser from triggering the form submit
+//         statusElm.append('<di>Email is not valid</div>')//Adds the new HTML to inform user
+//       }
+
+//       if(subject.length >= 2){
+//         statusElm.append('<div>Subject is valid</div>')
+//       } else {
+//         event.preventDefault()//stops browser from triggering the form submit
+//         statusElm.append('<div>Subject is not valid</div>')
+//       }
+
+//       if(message.length >= 10) {
+//         statusElm.append('<div>Subject is valid</div>')
+//       } else {
+//         event.preventDefault()//stops browser from triggering the form submit
+//         statusElm.append('<div>Subject is not valid</div>')
+//       }
+//    })
+// })
 
