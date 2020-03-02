55973360f1f53b29cb7f64e434ca26c66998f7be
Zavier <zmyles@comcast.net>
2020-03-01 21:14:35 -0500

    Website is now more mobile frendly, need to re touch porfile page and find fix mobile nav'

--- scripts.js
+++ scripts.js
@@ -42,79 +42,45 @@ $(document).ready(function(){
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//    $('.submit').click(function(event) {
+$(document).ready(function () {
+   $('.submit').click(function(event) {
      
      
-//      var email = $('.email').val()
-//      var subject = $('.subject').val()
-//      var message = $('.message').val()
-//      var statusElm = $('.status')
-//      statusElm.empty()//Empty's function so we always start from scratch
+     var email = $('.email').val()
+     var subject = $('.subject').val()
+     var message = $('.message').val()
+     var statusElm = $('.status')
+     statusElm.empty()//Empty's function so we always start from scratch
 
-//      if(email.length > 5 && email.includes('@') && email.includes('.')) {
-//        statusElm.append('<div>Email is valid</div>')//Adds the new HTML to inform user
-//       } else {
-//         event.preventDefault()//stops browser from triggering the form submit
-//         statusElm.append('<di>Email is not valid</div>')//Adds the new HTML to inform user
-//       }
+     if(email.length > 5 && email.includes('@') && email.includes('.')) {
+       statusElm.append('<div>Email is valid</div>')//Adds the new HTML to inform user
+      } else {
+        event.preventDefault()//stops browser from triggering the form submit
+        statusElm.append('<di>Email is not valid</div>')//Adds the new HTML to inform user
+      }
 
-//       if(subject.length >= 2){
-//         statusElm.append('<div>Subject is valid</div>')
-//       } else {
-//         event.preventDefault()//stops browser from triggering the form submit
-//         statusElm.append('<div>Subject is not valid</div>')
-//       }
+      if(subject.length >= 2){
+        statusElm.append('<div>Subject is valid</div>')
+      } else {
+        event.preventDefault()//stops browser from triggering the form submit
+        statusElm.append('<div>Subject is not valid</div>')
+      }
 
-//       if(message.length >= 10) {
-//         statusElm.append('<div>Subject is valid</div>')
-//       } else {
-//         event.preventDefault()//stops browser from triggering the form submit
-//         statusElm.append('<div>Subject is not valid</div>')
-//       }
-//    })
-// })
+      if(message.length >= 10) {
+        statusElm.append('<div>Subject is valid</div>')
+      } else {
+        event.preventDefault()//stops browser from triggering the form submit
+        statusElm.append('<div>Subject is not valid</div>')
+      }
+   })
+})
 
+
+
+
+
+
+
+function show() {
+  document.getElementById('sidebar').classList.toggle('active');
+}
